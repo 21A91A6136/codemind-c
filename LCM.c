a3f559386aef98ba5c858fcc80fa04dd7ea298a2
21A1A6136 <97614574+21A1A6136@users.noreply.github.com>
2022-05-03 10:35:28 +0530

    #include<stdio.h> int main() {     int a,b,max;     scanf("%d%d",&a,&b);     max=(a>b)?a:b;     while(1)     {         if(max%a==0&&max%b==0)         {             printf("%d",max);             break;         }         ++max;     }     return 0; }

---
+++ LCM.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,max;
+    scanf("%d%d",&a,&b);
+    max=(a>b)?a:b;
+    while(1)
+    {
+        if(max%a==0&&max%b==0)
+        {
+            printf("%d",max);
+            break;
+        }
+        ++max;
+    }
+    return 0;
+}
