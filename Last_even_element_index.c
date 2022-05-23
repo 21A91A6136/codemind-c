8964a40bbbe6d43282b931b24d9fc71587b99fad
21A1A6136 <97614574+21A1A6136@users.noreply.github.com>
2022-05-23 11:45:38 +0530

    #include<stdio.h> int main() {     int a[100],i,n,odd,ind;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             odd=a[i];             ind=i;         }     }     printf("%d",ind); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,n,odd,ind;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            odd=a[i];
+            ind=i;
+        }
+    }
+    printf("%d",ind);
+}
